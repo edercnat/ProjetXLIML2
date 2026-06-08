d69d0acb8725e5bfc19208cd4778beb1cfc57ad0
edercnat <plumedepaque@netc.fr>
2026-06-08 09:35:08 +0200

    Plus propre

--- serialisationClasse.js
+++ serialisationClasse.js
@@ -18,12 +18,12 @@ class Test {
 //Variables de test et globales
 const date = new Date(8.64e15);
 const set = new Set(["Vallet", "Dame", "Roi"]);
-const map = new Map([["clef1", 1], ["clef2", 2], [set, 157657651786n], [NaN, Infinity], [Infinity, -Infinity]]);
+const map = new Map([["clef1", 1], ["clef2", 2], [set, 157657651786n, 6.7], [NaN, Infinity], [Infinity, -Infinity]]);
 const regexp = new RegExp("mot");
 const chaine = "chaine";
 const test = new Test(1);
 const symbol =  Symbol("symbol");
-const erreur = new Error("Erreur de neuilllll", {cause : "rémi le gros aigri"});
+const erreur = new Error("Erreur de chad", {cause : "rémi le goat"});
 const objTest = {
     "date" : date,
     "map" : map,
@@ -55,13 +55,13 @@ function afficheAttributs(val){
 //Fonction d'affichage du dictionnaire des prototypes
 function afficheDicoProto(dico){
     for(const clef in dico){
-        console.log(clef);
+        console.log(clef, dico[clef]);
     }
 }
 
 //Fonction qui check si le prototype est déjà stocké dans le dico
 function estDejaStocke(name, dico){
-    for(clef in dico){
+    for(const clef in dico){
         if(clef == name){
             return true;
         }
@@ -81,46 +81,45 @@ function estTraiteNativement(obj){
 //-----------------------------------------------
 //  Fonction replacer
 //-----------------------------------------------
-/*
-function replacerSave(clef, valeur){
-    //Parfois valeur est déjà sérialisée (par exemple déjà en string pour une date)
-    //On utilise donc this[clef] (this représentant l'objet parent) pour récupérer la valeur non sérialisée
-    const objetOriginal = this[clef];
-    let valRetour = valeur;
 
-    console.log("Sérialisation :", objetOriginal.constructor.name, clef, valeur);
+// function replacerSave(clef, valeur){
+//     //Parfois valeur est déjà sérialisée (par exemple déjà en string pour une date)
+//     //On utilise donc this[clef] (this représentant l'objet parent) pour récupérer la valeur non sérialisée
+//     const objetOriginal = this[clef];
+//     let valRetour = valeur;
 
-    if(!estTraiteNativement(objetOriginal)){
-        if(clef != "valeur" && clef != "constructeur" && !Array.isArray(objetOriginal)){
-            //On ajoute la valeur dans le dico si elle n'a pas déjà été traitée
-            if(!estDejaStocke(valeur.constructor,DictionnairePrototypes)){
-                DictionnairePrototypes[this[clef].constructor.name] = Object.getPrototypeOf(objetOriginal);
-            }
-            return {
-                "constructeur" : objetOriginal.constructor.name,
-                "valeur" : valRetour
-            };
-        }
+//     console.log("Sérialisation :", objetOriginal.constructor.name, clef, valeur);
 
-        if(clef == "valeur"){            
-            valeurRetour = Array.from(objetOriginal);
-            if(valeurRetour.length > 0){
-                return Array.from(objetOriginal);
-            }
-            else{
-                if(typeof valeur != "object" || typeof valeur == "regexp"){
-                    valRetour = valeur.toString();
-                }
-            }
-        }
-    }
-    //On retourne la valeur de base si elle est traitée nativement ou déjà traité
-    return valRetour;
-}
-*/
+//     if(!estTraiteNativement(objetOriginal)){
+//         if(clef != "valeur" && clef != "constructeur" && !Array.isArray(objetOriginal)){
+//             //On ajoute la valeur dans le dico si elle n'a pas déjà été traitée
+//             if(!estDejaStocke(valeur.constructor,DictionnairePrototypes)){
+//                 DictionnairePrototypes[this[clef].constructor.name] = Object.getPrototypeOf(objetOriginal);
+//             }
+//             return {
+//                 "constructeur" : objetOriginal.constructor.name,
+//                 "valeur" : valRetour
+//             };
+//         }
+
+//         if(clef == "valeur"){            
+//             valeurRetour = Array.from(objetOriginal);
+//             if(valeurRetour.length > 0){
+//                 return Array.from(objetOriginal);
+//             }
+//             else{
+//                 if(typeof valeur != "object" || typeof valeur == "regexp"){
+//                     valRetour = valeur.toString();
+//                 }
+//             }
+//         }
+//     }
+//     //On retourne la valeur de base si elle est traitée nativement ou déjà traité
+//     return valRetour;
+// }
+
 
 function replacer(clef, valeur){
-    
     const objetOriginal = this[clef];
     
     //Si c'est nul et undefined, on renvoie la chaîne de caractères pour ne pas les ignorer et éviter les erreurs
@@ -132,7 +131,7 @@ function replacer(clef, valeur){
     }
     //Si la valeur n'est pas stockée dans notre registre des constructeurs, on l'ajoute
     if(!estDejaStocke(this[clef].constructor.name, DictionnairePrototypes)){
-        DictionnairePrototypes[objetOriginal.constructor.name] = objetOriginal.prototype
+        DictionnairePrototypes[objetOriginal.constructor.name] = Object.getPrototypeOf(objetOriginal);
     }
     //console.log("Sérialisation :", objetOriginal.constructor.name, clef, valeur);
 
@@ -140,8 +139,9 @@ function replacer(clef, valeur){
     //On utilise donc this[clef] (this représentant l'objet parent) pour récupérer la valeur non sérialisée
     let valRetour = valeur;
 
-    //On dit que les types de base qui ne sont pas à sérialiser sont les strings et les listes en js
-    if(typeof valeur != "string" && !Array.isArray(valeur) && Object.prototype.toString.call(valeur) != "[object Object]"){
+    //Nos types primitifs sont les strings, listes et les objets de type dictionnaire.
+    //On ne les traite donc pas
+    if(typeof objetOriginal != "string" && !Array.isArray(objetOriginal) && Object.prototype.toString.call(objetOriginal) != "[object Object]"){
 
         
 
@@ -156,14 +156,15 @@ function replacer(clef, valeur){
                 "valeur" : objetOriginal
             }
         }
-        //Si on est dans une valeur à traiter, on la traite selon ce que l'on veut
+
+        //Si on est dans une valeur à traiter (donc avec la clef "valeur"), on la traite selon ce que l'on veut
         else if(clef == "valeur"){
             let valSerialisee = Array.from(objetOriginal);
             //Si on peut la convertir en liste
             if(valSerialisee.length > 0){
                 valRetour = valSerialisee;
             }
-            //Sinon, on sérialise tout le reste sauf les object de type dictionnaires
+            //Sinon, on sérialise tout le reste sauf les object de type dictionnaire et les classes
             if(valSerialisee.length == 0 && valeur.constructor.name != "Object"){
                 valRetour = valeur.toString();
             }
